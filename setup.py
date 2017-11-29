82042e8ef1122bc25d8f0cb1c1c7a317b9847e4c
ofgulban <farukgulban@gmail.com>
2017-11-29 23:56:53 +0100

    Pep8 and pep 257 fixes. I have fixed most of the linter warnings except a few.

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 #
 # Copyright (C) 2013-2015 Russell Poldrack <poldrack@stanford.edu>
-# some portions borrowed from https://github.com/mwaskom/lyman/blob/master/setup.py
+#
+# Some portions borrowed from:
+# https://github.com/mwaskom/lyman/blob/master/setup.py
 
 
 descr = """pydeface: a script to remove facial structure from MRI images"""
@@ -10,20 +12,20 @@ import os
 from setuptools import setup
 import glob
 
-DISTNAME="pydeface"
-DESCRIPTION=descr
-MAINTAINER='Russ Poldrack'
-MAINTAINER_EMAIL='poldrack@stanford.edu'
-LICENSE='MIT'
-URL='http://poldracklab.org'
-DOWNLOAD_URL='https://github.com/poldracklab/pydeface/'
-VERSION='1.0'
+DISTNAME = "pydeface"
+DESCRIPTION = descr
+MAINTAINER = 'Russ Poldrack'
+MAINTAINER_EMAIL = 'poldrack@stanford.edu'
+LICENSE = 'MIT'
+URL = 'http://poldracklab.org'
+DOWNLOAD_URL = 'https://github.com/poldracklab/pydeface/'
+VERSION = '1.0'
+
 
 def check_dependencies():
-
     # Just make sure dependencies exist, I haven't rigorously
     # tested what the minimal versions that will work are
-    needed_deps = ["numpy", "nibabel","nipype"]
+    needed_deps = ["numpy", "nibabel", "nipype"]
     missing_deps = []
     for dep in needed_deps:
         try:
@@ -33,6 +35,7 @@ def check_dependencies():
 
     if missing_deps:
         raise ImportError("Missing dependencies: %s" % missing_deps)
+
 
 if __name__ == "__main__":
 
@@ -47,23 +50,24 @@ if __name__ == "__main__":
                             'clean'))):
         check_dependencies()
 
-    datafiles = {'pydeface': ['data/facemask.nii.gz','data/mean_reg2mean.nii.gz']}
+    datafiles = {'pydeface': ['data/facemask.nii.gz',
+                              'data/mean_reg2mean.nii.gz']}
 
     setup(name=DISTNAME,
-        maintainer=MAINTAINER,
-        maintainer_email=MAINTAINER_EMAIL,
-        description=DESCRIPTION,
-        license=LICENSE,
-        version=VERSION,
-        url=URL,
-        download_url=DOWNLOAD_URL,
-        packages=['pydeface'],
-        package_data  = datafiles,
-        scripts=['scripts/pydeface.py'],
-        classifiers=['Intended Audience :: Science/Research',
-                     'Programming Language :: Python :: 2.7',
-                     'License :: OSI Approved :: BSD License',
-                     'Operating System :: POSIX',
-                     'Operating System :: Unix',
-                     'Operating System :: MacOS'],
-    )
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          license=LICENSE,
+          version=VERSION,
+          url=URL,
+          download_url=DOWNLOAD_URL,
+          packages=['pydeface'],
+          package_data=datafiles,
+          scripts=['scripts/pydeface.py'],
+          classifiers=['Intended Audience :: Science/Research',
+                       'Programming Language :: Python :: 2.7',
+                       'License :: OSI Approved :: BSD License',
+                       'Operating System :: POSIX',
+                       'Operating System :: Unix',
+                       'Operating System :: MacOS']
+          )
