5234c34afe0c7ca8f450e4c1a86a012ed0facd7b
ofgulban <farukgulban@gmail.com>
2017-11-30 00:46:34 +0100

    Cleanup setup.py - Small version bump 1.1 -> 1.1. - Dependency check is changed to be done by setuptools.

--- setup.py
+++ setup.py
@@ -2,72 +2,43 @@
 #
 # Copyright (C) 2013-2015 Russell Poldrack <poldrack@stanford.edu>
 #
-# Some portions borrowed from:
+# Some portions were borrowed from:
 # https://github.com/mwaskom/lyman/blob/master/setup.py
-
-
-descr = """pydeface: a script to remove facial structure from MRI images"""
 
 import os
 from setuptools import setup
-import glob
 
 DISTNAME = "pydeface"
-DESCRIPTION = descr
+DESCRIPTION = "pydeface: a script to remove facial structure from MRI images."
 MAINTAINER = 'Russ Poldrack'
 MAINTAINER_EMAIL = 'poldrack@stanford.edu'
 LICENSE = 'MIT'
 URL = 'http://poldracklab.org'
 DOWNLOAD_URL = 'https://github.com/poldracklab/pydeface/'
-VERSION = '1.0'
+VERSION = '1.1'
 
+if os.path.exists('MANIFEST'):
+    os.remove('MANIFEST')
 
-def check_dependencies():
-    # Just make sure dependencies exist, I haven't rigorously
-    # tested what the minimal versions that will work are
-    needed_deps = ["numpy", "nibabel", "nipype"]
-    missing_deps = []
-    for dep in needed_deps:
-        try:
-            __import__(dep)
-        except ImportError:
-            missing_deps.append(dep)
+datafiles = {'pydeface': ['data/facemask.nii.gz',
+                          'data/mean_reg2mean.nii.gz']}
 
-    if missing_deps:
-        raise ImportError("Missing dependencies: %s" % missing_deps)
-
-
-if __name__ == "__main__":
-
-    if os.path.exists('MANIFEST'):
-        os.remove('MANIFEST')
-
-    import sys
-    if not (len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
-            sys.argv[1] in ('--help-commands',
-                            '--version',
-                            'egg_info',
-                            'clean'))):
-        check_dependencies()
-
-    datafiles = {'pydeface': ['data/facemask.nii.gz',
-                              'data/mean_reg2mean.nii.gz']}
-
-    setup(name=DISTNAME,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          license=LICENSE,
-          version=VERSION,
-          url=URL,
-          download_url=DOWNLOAD_URL,
-          packages=['pydeface'],
-          package_data=datafiles,
-          scripts=['scripts/pydeface.py'],
-          classifiers=['Intended Audience :: Science/Research',
-                       'Programming Language :: Python :: 2.7',
-                       'License :: OSI Approved :: BSD License',
-                       'Operating System :: POSIX',
-                       'Operating System :: Unix',
-                       'Operating System :: MacOS']
-          )
+setup(name=DISTNAME,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      description=DESCRIPTION,
+      license=LICENSE,
+      version=VERSION,
+      url=URL,
+      download_url=DOWNLOAD_URL,
+      packages=['pydeface'],
+      package_data=datafiles,
+      scripts=['scripts/pydeface.py'],
+      classifiers=['Intended Audience :: Science/Research',
+                   'Programming Language :: Python :: 2.7',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS'],
+      install_requires=['numpy', 'nibabel', 'nipype'],
+      )
