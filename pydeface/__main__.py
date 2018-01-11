61f61184e95dc2b0e0cd5639d9bb9d7fde937597
leej3 <johnleenimh@gmail.com>
2018-01-11 17:04:40 -0500

    remove singleton dim when creating outdata

--- pydeface/__main__.py
+++ pydeface/__main__.py
@@ -116,7 +116,7 @@ def main():
     # multiply mask by infile and save
     infile_img = load(infile)
     tmpfile_img = load(tmpfile)
-    outdata = infile_img.get_data() * tmpfile_img.get_data()
+    outdata = infile_img.get_data().squeeze() * tmpfile_img.get_data()
     outfile_img = Nifti1Image(outdata, infile_img.get_affine(),
                               infile_img.get_header())
     outfile_img.to_filename(outfile)
