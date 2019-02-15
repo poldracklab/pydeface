c4b3681244c291e91a0166dc099023529aea10ad
Shotgunosine <adenosine@gmail.com>
2019-02-15 15:25:41 -0500

    FIX Path syntax

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -26,25 +26,15 @@ def test_get_outfile_type():
 
 def test_deface_image():
     if which('fsl'):
-        test_img = (
-            Path(__file__).
-            parent.parent.
-            joinpath(
-                'tests',
-                'data',
-                'ds000031_sub-01_ses-006_run-001_T1w.nii.gz').as_posix())
-        follow_img = (
-            Path(__file__).
-            parent.parent.
-            joinpath(
-                'tests',
-                'data',
-                'ds000031_sub-01_ses-006_run-001_T1w_follower.nii.gz').as_posix())
+        test_img = (Path(__file__).parent.parent / 'tests' / 'data' /
+                    'ds000031_sub-01_ses-006_run-001_T1w.nii.gz').as_posix()
+        follow_img = (Path(__file__).parent.parent / 'tests' / 'data' /
+                      'ds000031_sub-01_ses-006_run-001_T1w_follower.nii.gz').as_posix()
         # From https://stackoverflow.com/a/7244263
         data_url = 'https://openneuro.org/crn/datasets/ds000031/files/sub-01:ses-006:anat:sub-01_ses-006_run-001_T1w.nii.gz'
         urllib.request.urlretrieve(data_url, test_img)
         copyfile(test_img, follow_img)
-        defacewf = pdu.make_deface_workflow(test_img,nocleanup=False,force=True)
+        defacewf = pdu.make_deface_workflow(test_img, nocleanup=False,force=True)
         defacewf = pdu.append_follower_wf(defacewf, [follow_img])
         deface_res = defacewf.run()
         pdu.cleanup_files(test_img, follow_img)
